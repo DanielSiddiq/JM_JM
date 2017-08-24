14f72f73abbb75ea5ba61b2bed2dcaebbf575527
Daniel Siddiq <daniel.sidix@gmail.com>
2017-08-24 16:52:23 +0700

    Add files via upload

---
+++ Latihan1.php
@@ -0,0 +1,18 @@
+<?PHP
+	echo "Hello world";
+	//ini comment single-line
+	#ini juga sama
+	/*ini comment 1
+	ini coment 2
+	ini coment 3
+	*/
+	$txt = "Hello world!";
+	$x = 5;
+	$y = 10.5;
+	
+	echo $txt;
+	echo "<br/>";
+	echo "Nilai x yaitu $x!";
+	echo "<br/>";
+	echo "Nilai y yaitu" . $y ;
+?>
